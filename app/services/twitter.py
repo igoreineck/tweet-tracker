324fe981dd417880f137e8b9fc66d2d9dda651fc
Igor Eineck <hauzip@gmail.com>
2020-02-13 22:47:55 -0300

    FIX: changing the structure of service for tweepy

--- app/services/twitter.py
+++ app/services/twitter.py
@@ -7,12 +7,12 @@ class TwitterService(object):
     def __init__(self):
         self._service = tweepy
         self._auth = None
-        self._keys = dict(
-            consumer_key=os.getenv('CONSUMER_KEY'),
-            consumery_key_secret=os.getenv('CONSUMER_KEY_SECRET'),
-            access_token=os.getenv('ACCESS_TOKEN'),
-            access_token_secret=os.getenv('ACCESS_TOKEN_SECRET')
-        )
+        self._keys = {
+            'consumer_key': str(os.getenv('CONSUMER_KEY')),
+            'consumery_key_secret': str(os.getenv('CONSUMER_KEY_SECRET')),
+            'access_token': str(os.getenv('ACCESS_TOKEN')),
+            'access_token_secret': str(os.getenv('ACCESS_TOKEN_SECRET'))
+        }
 
     def authenticate(self):
         self._auth = self._service.OAuthHandler(
@@ -23,10 +23,11 @@ class TwitterService(object):
             self._keys.get('access_token'),
             self._keys.get('access_token_secret')
         )
+        return self._service
 
     @property
-    def execute(self):
-        return self._service.API(self._auth)
+    def search(self):
+        return self._service.API(self._auth).search
 
-    def cursor(self, query, **params):
-        return self._service.Cursor(query, **params)
+    def cursor(self, query, quantity=10, **params):
+        return self._service.Cursor(query, **params).items(quantity)

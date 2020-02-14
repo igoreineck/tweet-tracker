c7ad19f051c01e81c42860fcf70c37a3692b1573
Igor Eineck <hauzip@gmail.com>
2020-02-13 22:49:08 -0300

    REFAC: removing backup for flaskenv and changing init

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,15 @@ def create_app():
         instance_relative_config=False,
         static_folder='static',
     )
+    app_settings(app)
+    register_blueprints(app)
+    return app
 
+def app_settings(app):
     app.config.from_object('config.DevelopmentConfig')
+    return app
+
+def register_blueprints(app):
     app.register_blueprint(home)
     app.register_blueprint(messages)
     return app

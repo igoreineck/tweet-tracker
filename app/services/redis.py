e7792d696c918c484cf5a93edd45167b5e5be983
Igor Eineck <hauzip@gmail.com>
2020-02-15 15:17:53 -0300

    FIX: updating services abstraction

--- app/services/redis.py
+++ app/services/redis.py
@@ -1,8 +1,8 @@
+from app.constants.redis import RedisConstants
 import os
 import redis
 
 class RedisService(object):
-
     def __init__(self):
         self._connection = redis.Redis(
             host=os.getenv('REDIS_HOST'),
@@ -11,6 +11,18 @@ class RedisService(object):
             charset='utf-8',
             decode_responses=True
         )
+        self._QUEUE_START_POSITION = 0
+        self._QUEUE_END_POSITION = -1
+        self._session = self._connect()
 
-    def connect(self):
+    def _connect(self):
         return self._connection
+
+    @property
+    def queue(self):
+        return self._session.lrange(RedisConstants.HASHTAGS.value,
+            self._QUEUE_START_POSITION, self._QUEUE_END_POSITION)
+
+    @queue.setter
+    def queue(self, value):
+        self._session.rpush(RedisConstants.HASHTAGS.value, value)
--- app/services/twitter.py
+++ app/services/twitter.py
@@ -3,31 +3,28 @@ import tweepy
 
 
 class TwitterService(object):
-
     def __init__(self):
-        self._service = tweepy
-        self._auth = None
+        self._ITEMS = 15
         self._keys = {
-            'consumer_key': str(os.getenv('CONSUMER_KEY')),
-            'consumery_key_secret': str(os.getenv('CONSUMER_KEY_SECRET')),
+            'api_key': str(os.getenv('API_KEY')),
+            'api_key_secret': str(os.getenv('API_KEY_SECRET')),
             'access_token': str(os.getenv('ACCESS_TOKEN')),
             'access_token_secret': str(os.getenv('ACCESS_TOKEN_SECRET'))
         }
 
     def authenticate(self):
-        self._auth = self._service.OAuthHandler(
-            self._keys.get('consumer_key'),
-            self._keys.get('consumer_key_secret')
+        self._auth = tweepy.OAuthHandler(
+            self._keys.get('api_key'),
+            self._keys.get('api_key_secret')
         )
         self._auth.set_access_token(
             self._keys.get('access_token'),
             self._keys.get('access_token_secret')
         )
-        return self._service
 
-    @property
     def search(self):
-        return self._service.API(self._auth).search
+        return tweepy.API(self._auth).search
 
-    def cursor(self, query, quantity=10, **params):
-        return self._service.Cursor(query, **params).items(quantity)
+    def cursor(self, *args, **params):
+        return tweepy.Cursor(*args, **params)\
+            .items(params.get('quantity', self._ITEMS))
